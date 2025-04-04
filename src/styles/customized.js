670cabfeb55812c0ffda620292320dddd60d9007
arthurbbarbosa <atsushithesushi@gmail.com>
2025-04-03 23:53:31 -0300

    feat: new customized class

--- src/styles/customized.js
+++ src/styles/customized.js
@@ -1,4 +1,5 @@
 module.exports = {
   'flex-to-center': 'display: flex; justify-content: center; align-items: center',
-  'grid-to-center': 'display: grid; place-items: center'
+  'grid-to-center': 'display: grid; place-items: center',
+  'hide-scrollbar': 'scrollbar-width: none'
 }
