e0a16e3c6018e988ecc51cb1096e1c0a3d35b3cc
arthurbbarbosa <atsushithesushi@gmail.com>
2025-04-03 23:46:05 -0300

    feat: add overflow auto

--- src/styles/classes/overflow.js
+++ src/styles/classes/overflow.js
@@ -4,12 +4,15 @@
  */
 module.exports = {
   'overflow-visible': 'overflow: visible',
+  'overflow-auto': 'overflow: auto',
   'overflow-hidden': 'overflow: hidden',
 
   'overflow-x-visible': 'overflow-x: visible',
+  'overflow-x-auto': 'overflow: auto',
   'overflow-x-hidden': 'overflow-x: hidden',
 
   'overflow-y-visible': 'overflow-y: visible',
+  'overflow-y-auto': 'overflow: auto',
   'overflow-y-hidden': 'overflow-y: hidden',
 
   'line-clamp-[value]': (value) => `line-clamp: ${value}`
